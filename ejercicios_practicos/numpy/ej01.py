55c2e65e93ffbe3753884020bd69963d9a12e2a3
emmanuelbg10 <emmanuelbgiraldo@gmail.com>
2025-12-04 18:25:19 +0000

    ejercicios 1.1 y 1.2 hecho

--- ejercicios_practicos/numpy/ej01.py
+++ ejercicios_practicos/numpy/ej01.py
@@ -1,5 +1,29 @@
 import numpy as np
 
-a = np.array([[1, 2, 3], [4, 5, 6]])
+# Crear una matriz 5x24 con temperaturas aleatorias entre 15ºC y 35ºC
+temperaturas = np.random.uniform(15, 35, (5, 24))
+print("Temperaturas:\n", np.round(temperaturas, 2))
 
-print(a.shape)
+# -------------------------------------------------------------------
+
+# Calcular la temperatura media de cada sensor
+medias_filas = np.mean(temperaturas, axis=1)
+medias_filas = np.round(medias_filas, 2)
+print("1º Media de filas:", medias_filas, "\n")
+# ---------------------------------------------
+
+# Identificar el sensor con mayor temperatura promedio
+sensor = np.argmax(medias_filas)
+print("2º La temperatura mas alta está en el sensor:", (sensor + 1))
+print()
+# ---------------------------------------------------------------
+
+# Encontrar las horas donde algun sensor superó los 30ºC
+horas_criticas = np.where(np.any(temperaturas > 30, axis=0))[0]
+print(horas_criticas.shape)
+print("3º Las horas donde algun sensor supero los 30 grados:", horas_criticas)
+# --------------------------------------------------------
+
+# Calcular la desviación estandar de cada sensor
+print("4º Desviación estandar de cada sensor:\n", np.std(temperaturas, axis=1))
+# -----------------------------------------------
--- ejercicios_practicos/numpy/ej02.py
+++ ejercicios_practicos/numpy/ej02.py
@@ -1,14 +1,35 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-miarray = np.random.uniform(15, 35, (5, 24))
+# 1º Imagen sintética de 100x100 píxeles entre 0 y 255 aleatorios
+image = np.random.randint(0, 256, size=(100, 100), dtype=np.uint8)
+plt.imshow(image, cmap="gray", vmin=0, vmax=255)
+plt.savefig("./ejercicios_practicos/numpy/imagenes/imagen.png")
+# -------------------------------------------------------------------
 
-medias_filas = np.mean(miarray, axis=1)
-medias_filas = np.round(medias_filas, 2)
+# 2º Filtro de inversion de colores
+filtro_inverso = 255 - image
 
-print("Media de filas:", medias_filas, "\n")
-sensor = np.argmax(medias_filas)
-print("La temperatura mas alta está en el sensor:", (sensor + 1))
-print()
+plt.imshow(filtro_inverso, cmap="gray", vmin=0, vmax=255)
+plt.savefig("./ejercicios_practicos/numpy/imagenes/imagen_inverso.png")
+# ---------------------------------------------------------------------
 
-horas_criticas = np.where(np.any(miarray > 30, axis=0))[0]
-print(horas_criticas)
+# 3º Aumentar brillo un 20%
+image_brillo = np.clip(image * 1.2, 0, 255).astype(np.uint8)
+plt.imshow(image_brillo, cmap="gray", vmin=0, vmax=255)
+plt.savefig("./ejercicios_practicos/numpy/imagenes/imagen_brillo.png")
+# ---------------------------------------------------------------------
+
+# 4º Extraer cuadrante superior izquierdo (50x50)
+cuadrante = image[:50, :50]
+plt.imshow(cuadrante, cmap="gray", vmin=0, vmax=255)
+plt.savefig("./ejercicios_practicos/numpy/imagenes/imagen_cuadrante.png")
+# -------------------------------------------------------------------------
+
+# 5º Mascara que identifica pixeles muy oscuros
+mask = image < 50
+
+# los pixeles oscuros los muestra en blanco y los demas en negro
+plt.imshow(mask.astype(np.uint8) * 255, cmap="gray")
+plt.savefig("./ejercicios_practicos/numpy/imagenes/imagen_mascara.png")
+# ----------------------------------------------------------------------
